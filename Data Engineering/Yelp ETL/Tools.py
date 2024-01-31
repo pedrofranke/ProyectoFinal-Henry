ebc6b05d43939b9cc213dd5ac03fbbdcc6220ebd
Maximiliano Tauil <135084020+MaximilianoTauil@users.noreply.github.com>
2024-01-31 18:13:08 -0300

    Add files via upload

--- Data Engineering/Yelp ETL/Tools.py
+++ Data Engineering/Yelp ETL/Tools.py
@@ -1,55 +1,106 @@
 import pandas as pd
 import jsonlines
 
-def analizar_datos(df):
-    
+def analyze_data(dataframe):
+    """
+    This function takes a DataFrame as input and returns another DataFrame with 5 columns:
+    1) Name: The name of the column.
+    2) Unique Data Types: The unique data types present in the column.
+    3) % of Non-null Values: Percentage of values that are not null.
+    4) % of Null Values: Percentage of values that are null.
+    5) Number of Null Values: Count of null values in the column.
+
+    Parameters:
+    DataFrame (pandas.DataFrame): The DataFrame to be processed.
+
+    Returns:
+    resume_dataframe (pandas.DataFrame): The resulting DataFrame after processing.
+    """
 
 
-    resumen_dict = {"Nombre": [], "Tipos de Datos Únicos": [], "% de Valores No Nulos": [], "% de Valores Nulos": [], "Cantidad de Valores Nulos": []}
+    resume_dict = {"Name": [], "Unique Data Types": [], "% of Non-null Values": [], "% of Null Values": [], "Number of Null Values": []}
 
-    for columna in df.columns:
-        porcentaje_no_nulos = (df[columna].count() / len(df)) * 100
-        resumen_dict["Nombre"].append(columna)
-        resumen_dict["Tipos de Datos Únicos"].append(df[columna].apply(type).unique())
-        resumen_dict["% de Valores No Nulos"].append(round(porcentaje_no_nulos, 2))
-        resumen_dict["% de Valores Nulos"].append(round(100 - porcentaje_no_nulos, 2))
-        resumen_dict["Cantidad de Valores Nulos"].append(df[columna].isnull().sum())
+    for column in dataframe.columns:
+        percentage_non_null = (dataframe[column].count() / len(dataframe)) * 100
+        resume_dict["Name"].append(column)
+        resume_dict["Unique Data Types"].append(dataframe[column].apply(type).unique())
+        resume_dict["% of Non-null Values"].append(round(percentage_non_null, 2))
+        resume_dict["% of Null Values"].append(round(100 - percentage_non_null, 2))
+        resume_dict["Number of Null Values"].append(dataframe[column].isnull().sum())
 
-    resumen_dataframe = pd.DataFrame(resumen_dict)
+    resume_dataframe = pd.DataFrame(resume_dict)
         
-    return resumen_dataframe
+    return resume_dataframe
 
-def cantidad_porcentaje(dataframe, columna):
+def count_and_percentage(dataframe, column):
+    """
+    This function processes a DataFrame and a specified column, 
+    and returns the count of occurrences for each unique value in the column
+    and the percentage representation of each value in the total data of that column.
+
+    Parameters:
+    dataframe (pandas.DataFrame): The DataFrame to be processed.
+    column (str): The column on which operations are performed.
+
+    Returns:
+    The function prints information for the specified column.
+    """
     
-    cantidad = dataframe.shape[0]
-    cantidad_columna = dataframe[columna].value_counts(dropna=False)
-    porcentaje_columna = round((cantidad_columna / cantidad) * 100, 2)
+    quantity = dataframe.shape[0]
+    quantity_column = dataframe[column].value_counts(dropna=False)
+    Percentage_column = round((quantity_column / quantity) * 100, 2)
     
-    print(f'Los valores de {columna}:\n{cantidad_columna.to_string(header=False)}')
-    print(f'\nEl porcentaje que representa cada valor:\n{porcentaje_columna.to_string(header=False)}')
+    print(f'The values of {column}:\n{quantity_column.to_string(header=False)}')
+    print(f'\nThe percentage that each value represents:\n{Percentage_column.to_string(header=False)}')
 
-def AbrirJsonYelp(file_path):
+def OpenJsonYelp(file_path):
+    """
+    This function takes a file path in JSON format and returns the file as a DataFrame.
 
+    Parameters:
+    file_path (str): The file path in JSON format.
+
+    Returns:
+    The function returns a DataFrame from the provided file.
+    """
     json_objects = []
 
     with jsonlines.open(file_path) as reader:
      for line in reader:
           json_objects.append(line)
 
-    df = pd.DataFrame(json_objects)
-    return df
+    dataframe = pd.DataFrame(json_objects)
+    return dataframe
 
-def duplicates(DataFrame):
+def duplicates(dataframe):
+    """
+    This function takes a DataFrame and returns a DataFrame with duplicate records.
 
-    if DataFrame.empty:
-        print(f"El DataFrame {DataFrame} no tiene duplicados")
+    Parameters:
+    dataframe (pandas.DataFrame): The DataFrame to be processed.
+
+    Returns:
+    duplicates (pandas.DataFrame): The resulting DataFrame after processing.
+    """
+    
+    if dataframe.empty:
+        print(f"The DataFrame {dataframe} has no duplicates")
         return {}
 
-    duplicates = DataFrame[DataFrame.duplicated(keep=False)]
+    duplicates = dataframe[dataframe.duplicated(keep=False)]
     
     return duplicates
 
 def nulls(DataFrame,column):
+    """
+    This function takes a DataFrame and a column to analyze, and returns a DataFrame if it has null values.
+
+    Parameters:
+    dataframe (pandas.DataFrame): The DataFrame to be processed.
+
+    Returns:
+    null (pandas.DataFrame): The resulting DataFrame after processing.
+    """
 
     null = DataFrame[DataFrame[column].isna()]
     null = pd.DataFrame(null)
@@ -59,6 +110,16 @@ def nulls(DataFrame,column):
     return null
 
 def empty_values(DataFrame,column):
+    """
+    This function takes a DataFrame and a column to analyze, and returns a DataFrame if it has empty values.
+
+    Parameters:
+    dataframe (pandas.DataFrame): The DataFrame to be processed.
+    column (str): The column on which operations are performed.
+    
+    Returns:
+    emptys (pandas.DataFrame): The resulting DataFrame after processing.
+    """
 
     emptys = [x for x in DataFrame[column] if x == '' or x is None]
 
