806a26e6e480ba3c5e31fd8a7d5f1e6646a944a9
Pedro Franke <pedrofranke92@gmail.com>
2024-02-07 20:08:07 -0300

    streamlit correction

--- my_env/Scripts/activate.bat
+++ my_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\pedro\OneDrive\Pedro\Aprender\Cursos\Programacion\Data Science\Soy Henry\Clases\Proyecto Final\ProyectoFinal-Henry\Machine Learning\my_env
+set VIRTUAL_ENV=C:\Users\pedro\OneDrive\Pedro\Aprender\Cursos\Programacion\Data Science\Soy Henry\Clases\Proyecto Final\ProyectoFinal-Henry\my_env
 
 if not defined PROMPT set PROMPT=$P$G
 
